874a42fe506311d9dd74aa3ee15b99e224a9ee71
Widya Yasa <108384245+Widyasa@users.noreply.github.com>
2024-04-21 23:35:58 +0800

    done

--- detail.js
+++ detail.js
@@ -1,5 +1,5 @@
 const app = new Vue({
-    el : '#detail-section',
+    el : '#vue',
     data : {
         detail : {
             imgDetail : '',
@@ -10,6 +10,53 @@ const app = new Vue({
             description : '',
             stock : ''
         },
+        footer: [
+            {
+                footerDesc :
+                    {
+                        desc : 'At Swift Clothes, we believe that everyone deserves to experience the joy of wearing Clothes.',
+                        fb : './img/fb.svg',
+                        wa : './img/wa.svg',
+                        tt : './img/tt.svg'
+                    }
+            },
+            {
+                footerMenu : [
+                    {
+                        title : [
+                            'Quick Link',
+                            'Our Categories',
+                            'Help & Guide',
+                        ],
+                        footerContent : [
+                            {
+                                quickLink: [
+                                    'Home',
+                                    'Product',
+                                    'About',
+                                    'testi'
+                                ],
+                                categories: [
+                                    'Casual',
+                                    'Tuxedo',
+                                    'Dress',
+                                    'Shirt'
+                                ],
+                                guide: [
+                                    'Terms & Conditions',
+                                    'User Guide',
+                                    'Privacy Policy',
+                                    'Contact Us'
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                copyright : 'Copyright © 2023 swift Clothes. All Right Reserved.'
+            }
+        ],
         sizeSelected : '',
         specialRequest : '-',
         quantity : 0,
@@ -21,41 +68,41 @@ const app = new Vue({
             {
                 id: 1,
                 image : 'https://img.freepik.com/premium-photo/hanging-beige-shirt-with-wood-hanger-wall_1339-158095.jpg?w=360',
-                name : 'baju x',
+                name : 'baju Kemeja Pink',
                 priceBefore : 20000,
                 priceDiscount : 15000,
                 description: 'lorem ipsum dolor sit amet',
                 stock : 10,
                 quantity : 0,
                 specs : [
-                    'tes',
-                    'tes2',
-                    'tes3',
+                    'ringan',
+                    'murah',
+                    'nyaman',
                 ],
                 size : [
-                    '40',
-                    '41',
-                    '42'
+                    'S',
+                    'M',
+                    'L'
                 ]
             },
             {
                 id: 2,
                 image : 'https://img.freepik.com/free-photo/trendy-top-design-mockup-presented-wooden-hanger_460848-14043.jpg?t=st=1713173805~exp=1713177405~hmac=d7482f6966a6174095e8fa2e6ca1c31656531c916343236cb193da49a5afabd3&w=740',
-                name : 'baju x',
+                name : 'baju Krem Korea',
                 priceBefore : 10000,
                 priceDiscount : 5000,
                 description: 'lorem ipsum dolor sit amet',
                 stock : 10,
                 quantity : 0,
                 specs : [
-                    'tes',
-                    'tes2',
-                    'tes3',
+                    'ringan',
+                    'murah',
+                    'nyaman',
                 ],
                 size : [
-                    '40',
-                    '41',
-                    '42'
+                    'S',
+                    'M',
+                    'L'
                 ]
             },
             {
@@ -68,136 +115,48 @@ const app = new Vue({
                 stock : 10,
                 quantity : 0,
                 specs : [
-                    'tes',
-                    'tes2',
-                    'tes3',
+                    'ringan',
+                    'murah',
+                    'nyaman',
                 ],
                 size : [
-                    '40',
-                    '41',
-                    '42'
+                    'S',
+                    'M',
+                    'L'
                 ]
             },
             {
                 id: 4,
                 image :'https://img.freepik.com/free-photo/trendy-top-design-mockup-presented-wooden-hanger_460848-13969.jpg?t=st=1713173413~exp=1713177013~hmac=52791f03f06bf902d3df074eecc15013bfc14e8c62103acfed4a8292ac6f8998&w=740',
-                name :'',
+                name :'Baju Kemeja Hawai',
                 priceBefore : 20000,
                 priceDiscount :6000,
                 description: 'lorem ipsum dolor sit amet',
                 stock : 10,
                 quantity : 0,
                 specs : [
-                    'tes',
-                    'tes2',
-                    'tes3'
+                    'ringan',
+                    'murah',
+                    'nyaman'
 
                 ],
                 size : [
-                    '40',
-                    '41',
-                    '42'
+                    'S',
+                    'M',
+                    'L'
                 ]
-            },
-            {
-                id: 5,
-                image :'https://img.freepik.com/free-photo/graphic-tshirt-trendy-design-mockup-presented-wooden-hanger_460848-13975.jpg?t=st=1713173613~exp=1713177213~hmac=ff70c8d9e8d77882057ca186da5cdd081bffbfdabff67c123e3015e5b8541ae9&w=740',
-                name :'',
-                priceBefore : 20000,
-                priceDiscount :6000,
-                description: 'lorem ipsum dolor sit amet',
-                stock : 10,
-                quantity : 0,
-                specs : [
-                    'tes',
-                    'tes2',
-                    'tes3'
-
-                ],
-                size : [
-                    '40',
-                    '41',
-                    '42'
-                ]
-                        
-            },
-            {
-                id: 6,
-                image :'',
-                name :'',
-                priceBefore : 20000,
-                priceDiscount :6000,
-                description: 'lorem ipsum dolor sit amet',
-                stock : 10,
-                quantity : 0,
-                specs : [
-                    'tes',
-                    'tes2',
-                    'tes3'
-
-                ],
-                size : [
-                    '40',
-                    '41',
-                    '42'
-                ]
-                
-             },
-            {
-                id: 7,
-                image :'',
-                name :'',
-                priceBefore : 20000,
-                priceDiscount :6000,
-                description: 'lorem ipsum dolor sit amet',
-                stock : 10,
-                quantity : 0,
-                specs : [
-                    'tes',
-                    'tes2',
-                    'tes3'
-
-                ],
-                size : [
-                    '40',
-                    '41',
-                    '42'
-                ],
-             },
-              {
-                id: 8,
-                image :'',
-                name :'',
-                priceBefore : 20000,
-                priceDiscount :6000,
-                description: 'lorem ipsum dolor sit amet',
-                stock : 10,
-                quantity : 0,
-                specs : [
-                    'tes',
-                    'tes2',
-                    'tes3'
-                ],
-                size : [
-                    '40',
-                    '41',
-                    '42'
-                ]
-                
-            },
-                  
-
+            },   
         ]
     },
     methods: {
-        seeDetail (id) {
-            this.detail.imgDetail = this.products[id].image
-            this.detail.nameDetail = this.products[id].name
-            this.detail.priceDetail = this.products[id].priceDiscount
-            this.detail.sizeDetail = this.products[id].size
-            this.detail.specsDetail = this.products[id].specs
-            this.detail.description = this.products[id].description
-            this.detail.stock = this.products[id].stock
+        seeDetail (index) {
+            this.detail.imgDetail = this.products[index].image
+            this.detail.nameDetail = this.products[index].name
+            this.detail.priceDetail = this.products[index].priceDiscount
+            this.detail.sizeDetail = this.products[index].size
+            this.detail.specsDetail = this.products[index].specs
+            this.detail.description = this.products[index].description
+            this.detail.stock = this.products[index].stock
         },
         plusQuantity () {
             this.quantity += 1
@@ -211,6 +170,8 @@ const app = new Vue({
             if(this.detail.priceDetail != 0 && this.sizeSelected != '' && this.detail.stock > this.quantity) {
             this.cart.push({name:this.detail.nameDetail, image:this.detail.imgDetail, price:this.detail.priceDetail*this.quantity, packaging:this.packaging, size:this.sizeSelected, quantity:this.quantity, request:this.specialRequest})
                 this.cartMessage = "Success Store to Cart"
+            } else {
+                this.cartMessage = "Failed Store to Cart"
             }
             this.sizeSelected = ''
             this.specialRequest = '-'
@@ -230,19 +191,19 @@ const app = new Vue({
         },
         buyProductByDetail() {
             window.location.href = "https://wa.me/6282147382985?text=Hallo%2C%20Saya%20ingin%20memesan%20%0AProduk%3A%20" 
-                                    + this.detail.nameDetail + "%0ANotes%3A%20" + this.specialRequest +
+                                    + this.detail.nameDetail + "%0ANoringan%3A%20" + this.specialRequest +
                                     "%0AJumlah%3A%20" + this.quantity + "%0A%0ATerimakasih"
         },
         buyProductByCart() {
-            let message = "" 
-            console.log(this.cart)
-
+            let message = []
             for (let i = 0; i < this.cart.length; i++ ) {
-                                message +=
-                                    this.cart[i].name + this.cart[i].request + this.cart[i].quantity 
+                message.push( "%20%0AProduk%3A%20" 
+                + this.cart[i].name + "%0ANoringan%3A%20" + this.cart[i].request +
+                "%0AJumlah%3A%20" + this.cart[i].quantity + "%0A")
               }
-            //   console.log(message)
-              window.location.href= "https://wa.me/6282147382985?text=Hallo%2C%20Saya%20ingin%20memesan%20%0AProduk%3A%20" + message
+
+              window.location.href = "https://wa.me/6282147382985?text=Hallo%2C%20Saya%20ingin%20memesan" + message + "%0A%0ATerimakasih"
+        
         },
 
     },
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Pembagian Tugas pengerjaan website ecommerce #
+## Widya Yasa ##
+- membuat fungsi vue untuk product(transaksi, cart, get product, see detail)
+- membantu Kak Fauzan dalam pembuatan footer dinamis
+- membuat page detail
+- membuat modal detail product
+- membuat modal cart
+
+## Kak Fauzan ## 
+- menambahkan home page
+- membuat footer menjadi dinamis
